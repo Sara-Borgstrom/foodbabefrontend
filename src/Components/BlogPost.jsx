37336666f615403c4e498fb6664b01486adc7ade
SaraBorg <50766676+SaraBorg@users.noreply.github.com>
2020-03-17 18:22:02 +0100

    Styling and placement

--- src/Components/BlogPost.jsx
+++ src/Components/BlogPost.jsx
@@ -13,16 +13,18 @@ export const BlogPost = () => {
       .then(res => res.json())
       .then(json => setPost(json));
   }, [post]);
+
   return (
 
     <article className='blog-container'>
       {post.map(post => (
         <>
           <div className='blog-box'>
+            <img src={post.imageUrl} alt="" className='blog-image' />
             <h3><span>{post.type}</span></h3>
             <h2><span>{post.title}</span></h2>
             <p><span>{post.description}</span></p>
-            <button onClick={post.link} className='link-button'>Find more</button>
+            <a href={post.link} className='site-link'>Find more</a>
           </div>
         </>
       ))}
